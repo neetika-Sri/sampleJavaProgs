8225ae77bd5c3cd20ebe16e5fc2f30816b254563
neetika-Sri <neetika.srivastava@gmail.com>
2016-07-04 16:47:59 +0530

    WIP HTML Parser implementation

--- src/main/java/HtmlParser.java
+++ src/main/java/HtmlParser.java
@@ -1,3 +1,9 @@
+import com.sun.deploy.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by srivnee on 04/07/2016.
  */
@@ -5,6 +11,31 @@ public class HtmlParser {
 
     public String parseHtml(String inputString) {
         //TODO: Implement logic to parse Html
-        return null;
+        String[] inputStringArr = splitOnDelm(inputString,"\n");
+        ArrayList<String> outputStringArr = new ArrayList<String>();
+        for(String inp:inputStringArr){
+            outputStringArr.add(parseHtmlLine(inp));
+        }
+
+        return StringUtils.join(outputStringArr, "\n");
+    }
+
+    private String parseHtmlLine(String inp) {
+        int count = 0;
+        String outputString = "None";
+        /*String openingTag = Pattern.quote("<")+"/^([^\\].*)/"+Pattern.quote(">");
+        Pattern openingTagPat = Pattern.compile(openingTag);
+        Matcher matcher = openingTagPat.matcher(inp);
+        while(matcher.find()){
+            System.out.println(matcher.group());
+            System.out.println(matcher.start());
+            System.out.println(matcher.end());
+
+        }*/
+        return outputString ;
+    }
+
+    private String[] splitOnDelm(String inputString, String s) {
+        return inputString.split(s);
     }
 }
